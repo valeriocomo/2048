fa863f924cd129cd184a7d7cd516fa4fd9343da8
Valerio Como <valeriocomo@gmail.com>
2017-01-17 21:46:15 +0100

    test configuration done

---
+++ test/test-2048.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var should = chai.should;
+var expect = chai.expect;
+
+describe('2048 test',() => {
+//https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha
+//http://mherman.org/blog/2015/09/10/testing-node-js-with-mocha-and-chai/
+    it('test configuration OK', () => { 
+        expect(true).to.equal(true);
+    });
+});
